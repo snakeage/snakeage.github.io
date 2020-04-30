2003ba60b4b70e4f059a2c3ca391b332297fedb7
Alexander <Snakeage@gmail.com>
2020-04-30 17:55:22 +0300

    Updates

---
+++ precache-manifest.3ad73d97e69966b42265d7ad8661321d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25048d740394c3976cc1cb8cb417605c",
+    "url": "/imperial-bank/index.html"
+  },
+  {
+    "revision": "dcdfc23fa22b0c5d9d62",
+    "url": "/imperial-bank/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b3d90ad41e593445343c",
+    "url": "/imperial-bank/static/js/2.c762393a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imperial-bank/static/js/2.c762393a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcdfc23fa22b0c5d9d62",
+    "url": "/imperial-bank/static/js/main.511f0793.chunk.js"
+  },
+  {
+    "revision": "fe1f292cb72346fc3465",
+    "url": "/imperial-bank/static/js/runtime-main.5c73f455.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imperial-bank/static/media/logo.5d5d9eef.svg"
+  }
+]);
